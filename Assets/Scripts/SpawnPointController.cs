c2a3cebbc88033cd0afc73e0a260e68e5ba1d1b1
José Miguel García <100842573+Joseemigc2411@users.noreply.github.com>
2023-04-23 21:38:25 +0200

    [Add] More enemies added
    Substituted random generators for fixed positions

---
+++ Assets/Scripts/SpawnPointController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using Unity.Mathematics;
+using UnityEngine;
+using Random = UnityEngine.Random;
+using System;
+
+
+public class SpawnPointController : MonoBehaviour
+{
+
+    
+}
