449159d736e592f12d838d775070b529b0b66ad8
Jose Miguel Garcia <josemigc2411@gmail.com>
2023-05-01 21:42:57 +0200

    Stablished CoinManager

---
+++ Assets/Scripts/CoinManager.cs
@@ -0,0 +1,46 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CoinManager : MonoBehaviour
+{
+    int currentCoins;
+
+    public TextMeshProUGUI currentCoinsText;
+
+    public bool firstGame = true;
+
+   
+    void Start()
+    {
+        if (firstGame)
+        {
+            PlayerPrefs.SetInt("Coins", currentCoins);
+            firstGame = false;
+        }
+        
+    }
+
+   
+    void Update()
+    {
+        currentCoins = PlayerPrefs.GetInt("monedas", 0);
+        currentCoinsText.text = currentCoins.ToString("0");
+    }
+
+    public void addCoins(int coinsGiven)
+    {
+        currentCoins = PlayerPrefs.GetInt("monedas", 0);
+        currentCoins += coinsGiven;
+        PlayerPrefs.SetInt("monedas", currentCoins);
+    }
+
+    public void takeCoins(int coinsTaken)
+    {
+        currentCoins = PlayerPrefs.GetInt("monedas", 0);
+        currentCoins -= coinsTaken;
+        PlayerPrefs.SetInt("monedas", currentCoins);
+    }
+}
